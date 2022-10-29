95c06d3e313911bbeff3b7ae3a0e83ec7ba095bc
Emmanuel Nwankwo <cnwankwom@gmail.com>
2022-10-29 12:39:33 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   const value = 'World!!!';
-  return <div>Hello {value}</div>;
+  return <div>Hello {value}. Azure Demo</div>;
 }
 
 export default App;
